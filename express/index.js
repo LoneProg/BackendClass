760a1d68e3f6bd05b0c0c6d53191c5679fff9ecc
hazey <wolfdon73@gmail.com>
2024-12-06 19:47:05 +0100

    added mongoose

--- express/index.js
+++ express/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,3 +10,7 @@ app.get('/', (req,res) => {
 app.listen(3000, () => {
   console.log('Listening on localhost:3000');
 })
+
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.4";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
